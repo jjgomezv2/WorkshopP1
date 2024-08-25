ae8676470b02cf09f2a919b7ceb343b5c53d52e0
Juan Jose Gomez <jjgomezv2@eafit.edu.co>
2024-08-25 09:25:01 -0500

    Movies statistics per year and per genre. Statistics section

--- movie/views.py
+++ movie/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 
 from .models import Movie
 
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
+
 # Create your views here.
 
 def home(request):
@@ -20,3 +25,81 @@ def home(request):
 def about(request):
     #return HttpResponse('<h1>Welcome to About page</h1>')
     return render(request, 'about.html')
+
+def statistics_view(request):
+    matplotlib.use('Agg')
+    years = Movie.objects.values_list('year', flat=True).distinct().order_by('year') # Obtiene todos los años de las películas
+    movie_counts_by_year = {}
+    for year in years:
+        if year:
+            movies_in_year = Movie.objects.filter(year=year)
+        else:
+            movies_in_year = Movie.objects.filter(year__isnull = True)
+            year = 'None'
+        count = movies_in_year.count()
+        movie_counts_by_year[year] = count
+    
+    bar_width = 0.5
+    bar_spacing = 0.5
+    bar_positions = range(len(movie_counts_by_year))
+    
+    # Crear gráfica de barras
+    plt.bar(bar_positions,  movie_counts_by_year.values(), width=bar_width, align='center')
+    # Personalización
+    plt.title('Movies per year')
+    plt.xlabel('Year')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90)
+    plt.subplots_adjust(bottom=0.3)
+    
+    # Guardar gráfica en objeto BytesIO
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+    
+    # Convertir gráfica a base64
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64encode(image_png)
+    graphic = graphic.decode('utf-8')
+    
+    # GRÁFICO CON CANTIDAD PELÍCULAS X GENERO
+    
+    # Obtener todas las películas
+    all_movies = Movie.objects.all()
+    # diccionario para la cantidad de películas por genero
+    movie_counts_by_genre = {}
+    # Filtrar las películas por genero y contar la cantidad de películas por genero
+    for movie in all_movies:
+        genre = movie.genre.split(',')[0].strip() if movie.genre else "None"
+        if genre in movie_counts_by_genre:
+            movie_counts_by_genre[genre] += 1
+        else:
+            movie_counts_by_genre[genre] = 1
+        
+    # Ancho de las barras
+    bar_width = 0.5
+    # Posiciones de las barras  
+    bar_positions = range(len(movie_counts_by_genre))
+    # Crear la gráfica de barras
+    plt.bar(bar_positions, movie_counts_by_genre.values(), width=bar_width, align='center')
+    
+    plt.title('Movies per genre')
+    plt.xlabel('Genre')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_genre.keys(), rotation=90)
+    plt.subplots_adjust(bottom=0.3)
+    
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+    # Convertir la gráfica a base64
+    image_png2 = buffer.getvalue()
+    buffer.close()
+    graphic2 = base64.b64encode(image_png2)
+    graphic2 = graphic2.decode('utf-8')
+
+    return render(request, 'statistics.html', {'graphic': graphic, 'graphic2': graphic2})
+    
